5f2b911c16db8ba3a890147b061b923591132e32
AngelikaGralewska <105511457+AngelikaGralewska@users.noreply.github.com>
2022-09-25 12:24:46 +0200

    Add files via upload

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", inputBlur);
+
+function inputBlur(event) {
+    
+    if (input.value.length === Number(input.dataset.length) ) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+
+    } else {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    }
+}
